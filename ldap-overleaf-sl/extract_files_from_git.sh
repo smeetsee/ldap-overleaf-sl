23dbda51668b75b9315a441dcd0211685ea155fd
Xander Smeets <xander@smeets.ee>
2024-06-10 10:18:21 +0200

    Fix navbar diff

--- ldap-overleaf-sl/extract_files_from_git.sh
+++ ldap-overleaf-sl/extract_files_from_git.sh
@@ -8,7 +8,7 @@ curl -o $OUTPUT_PREFIX"ContactController.js" $GIT_URL"services/web/app/src/Featu
 curl -o $OUTPUT_PREFIX"router.js" $GIT_URL"services/web/app/src/router.js" 
 curl -o $OUTPUT_PREFIX"settings.pug" $GIT_URL"services/web/app/views/user/settings.pug" 
 curl -o $OUTPUT_PREFIX"login.pug" $GIT_URL"services/web/app/views/user/login.pug" 
-curl -o $OUTPUT_PREFIX"navbar.pug" $GIT_URL"services/web/app/views/layout/navbar.pug" 
+curl -o $OUTPUT_PREFIX"navbar-website-redesign.pug" $GIT_URL"services/web/app/views/layout/navbar-website-redesign.pug" 
 curl -o $OUTPUT_PREFIX"navbar-marketing.pug" $GIT_URL"services/web/app/views/layout/navbar-marketing.pug" 
 curl -o $OUTPUT_PREFIX"admin-index.pug" $GIT_URL"services/web/app/views/admin/index.pug" 
 curl -o $OUTPUT_PREFIX"admin-sysadmin.pug" $GIT_URL"services/web/app/views/admin/index.pug" 
